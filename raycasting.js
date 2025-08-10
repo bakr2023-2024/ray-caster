3aae39c8ce8f7c188057d57e5696ad85a1d6048d
bakr2023-2024 <bakr2023.2024@gmail.com>
2025-08-10 05:57:00 +0300

    Add main loop

--- raycasting.js
+++ raycasting.js
@@ -1,6 +1,10 @@
 const screen = { width: window.innerWidth / 2, height: window.innerHeight / 2 };
 const player = { fov: 60, angle: 90, x: 2, y: 2 };
-const rayCastConfig = { incAngle: player.fov / screen.width, precision: 64 };
+const rayCastConfig = {
+  incAngle: player.fov / screen.width,
+  precision: 64,
+  delay: 30,
+};
 screen.hWidth = screen.width / 2;
 screen.hHeight = screen.height / 2;
 player.hFov = player.fov / 2;
@@ -9,3 +13,18 @@ canvas.width = screen.width;
 canvas.height = screen.height;
 document.body.appendChild(canvas);
 const g = canvas.getContext("2d");
+const rToD = (d) => (d * Math.PI) / 180;
+const drawLine = (x1, y1, x2, y2, color) => {
+  g.strokeStyle = color;
+  g.beginPath();
+  g.moveTo(x1, y1);
+  g.lineTo(x2, y2);
+  g.stroke();
+};
+const clearScreen = () => g.clearRect(0, 0, screen.width, screen.height);
+const rayCasting = () => {};
+const start = () =>
+  setInterval(() => {
+    clearScreen();
+    rayCasting();
+  }, rayCastConfig.delay);
